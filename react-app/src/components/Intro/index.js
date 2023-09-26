2dc228e588acd5290b19859ab670792e28d2d4b9
Son Hoang <sonquhoang@gmail.com>
2023-09-26 03:56:26 -0700

    Updating portfolio site

--- react-app/src/components/Intro/index.js
+++ react-app/src/components/Intro/index.js
@@ -6,19 +6,22 @@ const Intro = () => {
     return (
         <div className="intro-container">
             <div className="intro">
-            <span className="intro-name">Sean Hoang</span>
-            <br></br>
-            Full-Stack{" "}
-            <span class="intro-role">Software Engineer</span>
-            .
-            <p className="intro-speech">
-                <div>
-                Finding solutions
-                </div>
-                <div>
-                one problem at a time.
-                </div>
-            </p>
+            <div className="profile-image">
+                <img src="https://res.cloudinary.com/dgxpqnbwn/image/upload/c_thumb,w_200,g_face/v1695725317/IMG_3824_1_k5epnr.jpg" alt="young man smiling at dinner" />
+            </div>
+                <span className="intro-name">Sean Hoang</span>
+                <br></br>
+                Full-Stack{" "}
+                <span class="intro-role">Software Engineer</span>
+                .
+                <p className="intro-speech">
+                    <div>
+                        Finding solutions
+                    </div>
+                    <div>
+                        one problem at a time.
+                    </div>
+                </p>
             </div>
         </div>
     )
